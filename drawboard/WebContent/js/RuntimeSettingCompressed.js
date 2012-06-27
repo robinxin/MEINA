a613b5d4463e0d67a07d6e091583d0bf164b8755
Robinlim <vicxin1010@hotmail.com>
2012-06-27 23:05:39 +0800

    version MEINA

---
+++ drawboard/WebContent/js/RuntimeSettingCompressed.js
@@ -0,0 +1,6 @@
+var drawboardRuntimeSetting = {
+	blankGif:"images/blank.gif",
+	toolbarImgUrl:"js/data/Toolbar.json",
+    graphImgUrl:"js/data/GraphClassify.json",
+	commands:"js/data/Command.json"
+};
